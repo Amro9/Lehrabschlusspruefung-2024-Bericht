7114921aa5aa035ed68a2e32deff604787aaac0c
Amro9 <81911920+Amro9@users.noreply.github.com>
2024-07-12 16:29:54 +0200

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/12.07/components/create/form.php">Add user</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/12.07/components/readAll/userTable.php">List users</a>
+      </li>
+    </ul>
+  </div>
+</nav>
